3a6d462ae7c265b5a8b47bd8197c8add6f13ce55
Par_Group <bluekkid@gmail.com>
2017-12-05 02:53:03 +0000

    Hey look the valid move func doesn't work right

--- mazeSolver.pl
+++ mazeSolver.pl
@@ -15,6 +15,30 @@ findPath(Start, Dest, [Head|Tail]) :-
 	findPath(Head, Dest, Tail).
 	
 
+edge(1,2).
+edge(1,4).
+edge(1,3).
+edge(2,3).
+edge(2,5).
+edge(3,4).
+edge(3,5).
+edge(4,5).
+
+connected(X,Y) :- edge(X,Y).
+connected(X,Y) :- edge(Y,X).
+
+path(A,B,Path) :-
+       travel(A,B,[A],Q), 
+       reverse(Q,Path).
+
+travel(A,B,P,[B|P]) :- 
+       adj_square(A,B).
+travel(A,B,Visited,Path) :-
+       adj_square(A,C),           
+       C \== B,
+       \+member(C,Visited),
+       travel(C,B,[C|Visited],Path).
+
 
 natural(1).
 natural(N) :- natural(M), N is M+1.
@@ -66,10 +90,10 @@ eGOAL(I, X, Y) :- button(X,Y, I).
 	
 
 /* Check if the two squares are adjacent */
-adj_square([X0,Y0], [X0,Y1]) :- (Y1 is Y0-1).
-adj_square([X0,Y0], [X0,Y1]) :- (Y1 is Y0+1).
-adj_square([X0,Y0], [X1,Y0]) :- (X1 is X0-1).
-adj_square([X0,Y0], [X1,Y0]) :- (X1 is X0+1).
+adj_square([X0,Y0], [X0,Y1]) :- (Y1 is Y0-1), inside_board(X0,Y1), \+ wall(X0,Y1).
+adj_square([X0,Y0], [X0,Y1]) :- (Y1 is Y0+1), inside_board(X0,Y1), \+ wall(X0,Y1).
+adj_square([X0,Y0], [X1,Y0]) :- (X1 is X0-1), inside_board(X1,Y0), \+ wall(X1,Y0).
+adj_square([X0,Y0], [X1,Y0]) :- (X1 is X0+1), inside_board(X1,Y0), \+ wall(X1,Y0).
 
 
 /* The square is traversable (not blocked by wall) */
@@ -99,9 +123,11 @@ atButton(X,Y, ID) :-
 
 /* Move iff the square we want to move to is adjacent & not traversed & traversable(inside the board & not a wall) */
 valid_move([X0,Y0], [X1,Y1]) :- 
-	adj_square([X0,Y0], [X1,Y1]),  
+	traversable_square(X1,Y1),
+	adj_square([X0,Y0], [X1,Y1]).
 	/* traversed([X1,Y1]), */
-	traversable_square(X1,Y1).
+	/* traversed([X1,Y1]), */
+	
 	
 
 	
